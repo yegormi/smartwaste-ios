e2a0cddb752477eb7ed4c3b17f0021f4ce2dda54
Yegor Myropoltsev <72651640+yegormi@users.noreply.github.com>
2023-11-14 14:50:04 +0200

    Initial Commit

---
+++ SmartWaste/SmartWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartWasteApp.swift
+//  SmartWaste
+//
+//  Created by Yegor Myropoltsev on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
