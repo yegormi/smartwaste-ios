3959aaf15f9e132d27bbea813602ab1e4a2bc531
Yegor Myropoltsev <72651640+yegormi@users.noreply.github.com>
2023-11-15 17:32:58 +0200

    added images

--- SmartWaste/Features/Map/Views/MapViewRepresentable.swift
+++ SmartWaste/Features/Map/Views/MapViewRepresentable.swift
@@ -21,8 +21,48 @@ struct MapViewRepresentable: UIViewRepresentable {
         
         /// showing annotation on the map
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            guard let annotation = annotation as? LandmarkAnnotation else { return nil }
-            return AnnotationView(annotation: annotation, reuseIdentifier: AnnotationView.ReuseID)
+            if let cluster = annotation as? MKClusterAnnotation {
+                return ClusterAnnotationView(
+                    annotation: cluster,
+                    reuseIdentifier: ClusterAnnotationView.ReuseID
+                )
+            } else {
+                guard let annotation = annotation as? LandmarkAnnotation else { return nil }
+                return ClusterAnnotationView(
+                    annotation: annotation,
+                    reuseIdentifier: ClusterAnnotationView.ReuseID
+                )
+            }
+        }
+        
+        func mapView(_ mapView: MKMapView, clusterAnnotationForMemberAnnotations memberAnnotations: [MKAnnotation]) -> MKClusterAnnotation {
+            let cluster = MKClusterAnnotation(memberAnnotations: memberAnnotations)
+            cluster.title = ""
+            cluster.subtitle = ""
+            return cluster
+        }
+        
+        func resizeImage(image: UIImage?, targetSize: CGSize) -> UIImage {
+            guard let image = image else { return UIImage() }
+            
+            let size = image.size
+            let widthRatio  = targetSize.width  / size.width
+            let heightRatio = targetSize.height / size.height
+            
+            var newSize: CGSize
+            if widthRatio > heightRatio {
+                newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+            } else {
+                newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+            }
+            
+            let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+            UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+            image.draw(in: rect)
+            let newImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return newImage ?? UIImage()
         }
     }
     
@@ -60,16 +100,17 @@ class LandmarkAnnotation: NSObject, MKAnnotation {
     }
 }
 
-/// here posible to customize annotation view
 let clusterID = "clustering"
 
-class AnnotationView: MKMarkerAnnotationView {
-    static let ReuseID = "cultureAnnotation"
+class ClusterAnnotationView: MKMarkerAnnotationView {
+    static let ReuseID = "clusterAnnotation"
     
-    /// setting the key for clustering annotations
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        displayPriority = .defaultLow
         clusteringIdentifier = clusterID
+        collisionMode = .circle
+        markerTintColor = .systemGreen
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -78,6 +119,6 @@ class AnnotationView: MKMarkerAnnotationView {
     
     override func prepareForDisplay() {
         super.prepareForDisplay()
-        displayPriority = .defaultLow
+        canShowCallout = true
     }
 }
